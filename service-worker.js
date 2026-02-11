4dfb45a0cfa0c1c24ea9489976edf1ef218eeb65
petersonbritoarte-code <petersonbrito.arte@gmail.com>
2026-02-11 13:57:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "freq-2020-v1.6";
+const CACHE_NAME = "freq-2020-v1.7";
 const ASSETS = [
   "./",
   "./index.html",
@@ -38,6 +38,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
