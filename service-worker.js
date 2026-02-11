afeb6520d07b19fb5c858c94830231773babbcec
petersonbritoarte-code <petersonbrito.arte@gmail.com>
2026-02-11 08:15:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sweep-pwa-v2";
+const CACHE_NAME = "sweep-pwa-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -39,4 +39,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
